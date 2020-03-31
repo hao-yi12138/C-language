90f09ecbb22b07a2fa20d1596246db42f231b618
hao-yi12138 <692180261@qq.com>
2020-03-31 22:09:19 +0800

    3-28 数组

---
+++ haoyi3-28/haoyi3-28/test.c
@@ -0,0 +1,116 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//void BubbleSort(int arr[], int sz)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		int flag = 1;
+//		for (j = 0; j <sz - 1 - i; j++)
+//		{
+//			if (arr[j]>arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//				flag = 0;
+//			}
+//		}
+//		if (flag == 1)
+//		{
+//			break;
+//		}
+//	}
+//}
+//void PrintArr(int *arr, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz ; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	printf("ÇëÊäÈë10¸öÊý×Ö£º");
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	BubbleSort(arr, sz);
+//	PrintArr(arr, sz);
+//	return 0;
+//}
+//void Init(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = 0;
+//	}
+//}
+//void Print(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//void reverse(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	int tmp = 0;
+//	while (left<right)
+//	{
+//		tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[10] = {0};
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	Init(arr,sz);
+//	Print(arr, sz);
+//	reverse(arr, sz);
+//	return 0;
+//}
+void Print(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+int main()
+{
+	int arr1[] = { 1, 2, 3, 4, 5 };
+	int arr2[] = { 5, 6, 7, 8, 9 };
+	int tmp = 0;
+	int i = 0;
+	int sz = sizeof(arr1) / sizeof(arr1[0]);
+	for (i = 0; i < sz; i++)
+	{
+		tmp = arr1[i];
+		arr1[i] = arr2[i];
+		arr2[i] = tmp;
+	}
+	Print(arr1, sz);
+	Print(arr2, sz);
+
+	return 0;
+}
